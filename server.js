725a8fa13ede694e410633366aaae1fcc1a66900
lucas <vrnephy@gmail.com>
2023-11-21 19:15:18 +0100

    auteur

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require ( 'express')
+const express = require('express')
 const app = express()
-const expressLayouts = require ( 'express-ejs-layouts' )
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
-const indexRouter = require ( './routes/index' )
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-const mongoose = require ( 'mongoose' )
+const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/Biblio')
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.on('open', error => console.log('connected to mongoose'))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
-app.listen(process.env.PORT || 3003)
+app.listen(3003)
