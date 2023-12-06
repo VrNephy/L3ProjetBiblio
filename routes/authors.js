4104d496b13b711ed30fa39f7fde31f8eaf5d5b8
lucas <vrnephy@gmail.com>
2023-12-06 14:41:55 +0100

    validation commit

--- routes/authors.js
+++ routes/authors.js
@@ -1,109 +1,181 @@
-const express = require('express')
-const router = express.Router()
-const Author = require('../models/author')
-const Book = require('../models/book')
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
+const Author = require('../models/author');
+const Book = require('../models/book');
 
 // Authors Routes
 router.get('/', async (req, res) => {
-  let searchOptions = {}
+  let searchOptions = {};
   if (req.query.name != null && req.query.name !== '') {
-    searchOptions.name = new RegExp(req.query.name, 'i')
+    searchOptions.name = new RegExp(req.query.name, 'i');
   }
   try {
-    const authors = await Author.find(searchOptions)
+    const authors = await Author.find(searchOptions);
     res.render('authors/index', {
       authors: authors,
-      searchOptions: req.query
-    })
+      searchOptions: req.query,
+    });
   } catch {
-    res.redirect('/')
+    res.redirect('/');
   }
-})
+});
 
 // New Author Route
 router.get('/new', (req, res) => {
-  res.render('authors/new', { author: new Author() })
-})
+  res.render('authors/new', { author: new Author() });
+});
 
-// Créer Author Route
-router.post('/', async (req, res) => {
+// Create Author Route
+router.post('/', [
+  body("name", "Name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("first_name", "First name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name", "Family name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+], async (req, res) => {
   const author = new Author({
     name: req.body.name,
     first_name: req.body.first_name,
     family_name: req.body.family_name,
     date_of_birth: req.body.date_of_birth,
     date_of_death: req.body.date_of_death,
+  });
 
-  })
   try {
-    const newAuthor = await author.save()
-    res.redirect(`authors/${newAuthor.id}`)
+    const newAuthor = await author.save();
+    res.redirect(`authors/${newAuthor.id}`);
   } catch {
     res.render('authors/new', {
       author: author,
-      errorMessage: 'Error creating Author'
-    })
+      errorMessage: 'Error creating Author',
+    });
   }
-})
+});
 
 router.get('/:id', async (req, res) => {
   try {
-    const author = await Author.findById(req.params.id)
-    const books = await Book.find({ author: author.id }).exec()
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: author.id }).exec();
     res.render('authors/show', {
       author: author,
-      booksByAuthor: books
-    })
+      booksByAuthor: books,
+    });
   } catch {
-    res.redirect('/')
+    res.redirect('/');
   }
-})
+});
 
 router.get('/:id/edit', async (req, res) => {
   try {
-    const author = await Author.findById(req.params.id)
-    res.render('authors/edit', { author: author })
+    const author = await Author.findById(req.params.id);
+    res.render('authors/edit', { author: author });
   } catch {
-    res.redirect('/authors')
+    res.redirect('/authors');
   }
-})
+});
 
-router.put('/:id', async (req, res) => {
-  let author
+router.put('/:id', [
+  body("name", "Name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("first_name", "First name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name", "Family name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+], async (req, res) => {
+  let author;
+
   try {
-    author = await Author.findById(req.params.id)
-    author.name = req.body.name
-    author.first_name = req.body.first_name
-    author.family_name = req.body.family_name
-    author.date_of_birth = req.body.date_of_birth
-    author.date_of_death = req.body.date_of_death
-    await author.save()
-    res.redirect(`/authors/${author.id}`)
-  } catch {
-    if (author == null) {
-      res.redirect('/')
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    author.first_name = req.body.first_name;
+    author.family_name = req.body.family_name;
+    author.date_of_birth = req.body.date_of_birth;
+    author.date_of_death = req.body.date_of_death;
+
+    // Validation result
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form page with error messages.
+      renderEditPage(res, author, true, errors.array());
+      return;
+    }
+
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch (error) {
+    console.error(error);
+    if (author != null) {
+      renderEditPage(res, author, true);
     } else {
-      res.render('authors/edit', {
-        author: author,
-        errorMessage: 'Error updating Author'
-      })
+      res.redirect('/');
     }
   }
-})
+});
 
 router.delete('/:id', async (req, res) => {
-  let author
+  let author;
   try {
-    author = await Author.findById(req.params.id)
-    await author.remove()
-    res.redirect('/authors')
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect('/authors');
   } catch {
     if (author == null) {
-      res.redirect('/')
+      res.redirect('/');
     } else {
-      res.redirect(`/authors/${author.id}`)
+      res.redirect(`/authors/${author.id}`);
     }
   }
-})
+});
 
-module.exports = router
+async function renderEditPage(res, author, hasError = false, errors = []) {
+  try {
+    res.render('authors/edit', {
+      author: author,
+      errors: errors,
+      errorMessage: hasError ? 'Error Updating Author' : null,
+    });
+  } catch {
+    res.redirect('/authors');
+  }
+}
+
+module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const Book = require('../models/book')
-const Author = require('../models/author')
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 // All Books Route
 router.get('/', async (req, res) => {
-  let query = Book.find()
+  let query = Book.find();
   if (req.query.title != null && req.query.title != '') {
-    query = query.regex('title', new RegExp(req.query.title, 'i'))
+    query = query.regex('title', new RegExp(req.query.title, 'i'));
   }
   try {
-    const books = await query.exec()
+    const books = await query.exec();
     res.render('books/index', {
       books: books,
-      searchOptions: req.query
-    })
+      searchOptions: req.query,
+    });
   } catch {
-    res.redirect('/')
+    res.redirect('/');
   }
-})
+});
 
 // New Book Route
 router.get('/new', async (req, res) => {
-  renderNewPage(res, new Book())
-})
+  renderNewPage(res, new Book());
+});
 
 // Create Book Route
 router.post('/', async (req, res) => {
@@ -32,106 +33,136 @@ router.post('/', async (req, res) => {
     author: req.body.author,
     summary: req.body.summary,
     isbn: req.body.isbn,
-  })
+  });
 
   try {
-    const newBook = await book.save()
-    res.redirect(`books/${newBook.id}`)
+    const newBook = await book.save();
+    res.redirect(`books/${newBook.id}`);
   } catch {
-    renderNewPage(res, book, true)
+    renderNewPage(res, book, true);
   }
-})
+});
 
 // Show Book Route
 router.get('/:id', async (req, res) => {
   try {
-    const book = await Book.findById(req.params.id)
-                           .populate('author')
-                           .exec()
-    res.render('books/show', { book: book })
+    const book = await Book.findById(req.params.id).populate('author').exec();
+    res.render('books/show', { book: book });
   } catch {
-    res.redirect('/')
+    res.redirect('/');
   }
-})
+});
 
 // Edit Book Route
 router.get('/:id/edit', async (req, res) => {
   try {
-    const book = await Book.findById(req.params.id)
-    renderEditPage(res, book)
+    const book = await Book.findById(req.params.id);
+    renderEditPage(res, book);
   } catch {
-    res.redirect('/')
+    res.redirect('/');
   }
-})
+});
 
 // Update Book Route
-router.put('/:id', async (req, res) => {
-  let book
+router.put(
+  '/:id',
+  [
+    // Validate and sanitize fields.
+    body('title', 'Title must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('author', 'Author must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('summary', 'Summary must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('isbn', 'ISBN must not be empty').trim().isLength({ min: 1 }).escape(),
+  ],
+  async (req, res) => {
+    let book;
 
-  try {
-    book = await Book.findById(req.params.id)
-    book.title = req.body.title
-    book.author = req.body.author
-    book.summary = req.body.summary
-    book.isbn = req.body.isbn
-    await book.save()
-    res.redirect(`/books/${book.id}`)
-  } catch {
-    if (book != null) {
-      renderEditPage(res, book, true)
-    } else {
-      redirect('/')
+    try {
+      book = await Book.findById(req.params.id);
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.summary = req.body.summary;
+      book.isbn = req.body.isbn;
+
+      // Validation result
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        // There are errors. Render the form page with error messages.
+        renderEditPage(res, book, true, errors.array());
+        return;
+      }
+
+      await book.save();
+      res.redirect(`/books/${book.id}`);
+    } catch (error) {
+      console.error(error);
+      if (book != null) {
+        renderEditPage(res, book, true);
+      } else {
+        res.redirect('/');
+      }
     }
   }
-})
+);
 
 // Delete Book Page
 router.delete('/:id', async (req, res) => {
-  let book
+  let book;
   try {
-    book = await Book.findById(req.params.id)
-    await book.remove()
-    res.redirect('/books')
+    book = await Book.findById(req.params.id);
+    await book.remove();
+    res.redirect('/books');
   } catch {
     if (book != null) {
       res.render('books/show', {
         book: book,
-        errorMessage: 'Could not remove book'
-      })
+        errorMessage: 'Could not remove book',
+      });
     } else {
-      res.redirect('/')
+      res.redirect('/');
     }
   }
-})
+});
 
 async function renderNewPage(res, book, hasError = false) {
-  renderFormPage(res, book, 'new', hasError)
+  renderFormPage(res, book, 'new', hasError);
 }
 
-async function renderEditPage(res, book, hasError = false) {
-  renderFormPage(res, book, 'edit', hasError)
+async function renderEditPage(res, book, hasError = false, errors = []) {
+  try {
+    const authors = await Author.find({});
+    const params = {
+      authors: authors,
+      book: book,
+      errors: errors,
+    };
+    if (hasError) {
+      params.errorMessage = 'Error Updating Book';
+    }
+    res.render('books/edit', params);
+  } catch {
+    res.redirect('/books');
+  }
 }
 
 async function renderFormPage(res, book, form, hasError = false) {
   try {
-    const authors = await Author.find({})
+    const authors = await Author.find({});
     const params = {
       authors: authors,
-      book: book
-    }
+      book: book,
+    };
     if (hasError) {
       if (form === 'edit') {
-        params.errorMessage = 'Error Updating Book'
+        params.errorMessage = 'Error Updating Book';
       } else {
-        params.errorMessage = 'Error Creating Book'
+        params.errorMessage = 'Error Creating Book';
       }
     }
-    res.render(`books/${form}`, params)
+    res.render(`books/${form}`, params);
   } catch {
-    res.redirect('/books')
+    res.redirect('/books');
   }
 }
 
-
-
-module.exports = router
+module.exports = router;
